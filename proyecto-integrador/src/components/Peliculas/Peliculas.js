71f9f52d1d918d1418096329d774ecc36d3344b9
Your Name <you@example.com>
2021-09-08 19:37:06 -0300

    csss :(

--- proyecto-integrador/src/components/Peliculas/Peliculas.js
+++ proyecto-integrador/src/components/Peliculas/Peliculas.js
@@ -13,7 +13,7 @@ constructor(){
 
 componentDidMount(){
     const url = 'https://api.themoviedb.org/3/movie/popular?api_key=3db55afa4c61183073e97b76636daba5&language=en-US&page=2'
-    console.log(url)
+
     fetch(url)
         .then((respuesta) => respuesta.json())
         .then((data) => {
@@ -23,18 +23,18 @@ componentDidMount(){
                 cargando: true
             })
         })
-        .catch(err => console.log(err));
+    .catch(err => console.log(err));
 }
 
 borrarTarjeta(id){
-const resto = this.state.peliculas.filter((peliBorrada) => peliBorrada.id !== id)
-this.setState({
-    peliculas: resto,
-})
+    const resto = this.state.peliculas.filter((peliBorrada) => peliBorrada.id !== id)
+    this.setState({
+        peliculas: resto,
+    })
 }
 
-Agregar(){
-    const url = 'https://api.themoviedb.org/3/movie/popular?api_key=3db55afa4c61183073e97b76636daba5&language=en-US&page=3'
+agregar(){
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=3db55afa4c61183073e97b76636daba5&language=en-US&page=${1}`
     console.log(url)
     fetch(url)
         .then((respuesta) => respuesta.json())
@@ -49,15 +49,14 @@ Agregar(){
 
 render(){
     return(
-
         <React.Fragment>
-         {this.state.cargando === false ? (<iframe src="https://giphy.com/embed/xTkcEQACH24SMPxIQg" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>) : ( <article className='contenedor-tarjetas'>
+         {this.state.cargando === false ? (<iframe src="https://giphy.com/embed/xTkcEQACH24SMPxIQg" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>) : ( <article className='contenedor-tarjetas'>
          {this.state.peliculas.map((peliculas) => (
             <Tarjetas key={peliculas.id} id={peliculas.id} img={peliculas.poster_path} title={peliculas.title} descrip={peliculas.overview} estreno={peliculas.release_date} popularidad={peliculas.popularity} idioma={peliculas.original_language} borrar={(peliculaBorrada)=>this.borrarTarjeta(peliculaBorrada)}/>
         ))}
         </article>
         )} 
-        <button type="button" onClick={()=>this.Agregar()}>Cargar más tarjetas</button>
+        <button className="cargarMas" type="button" onClick={()=>this.agregar()}>Cargar más tarjetas</button>
         </React.Fragment>
     )
 }
